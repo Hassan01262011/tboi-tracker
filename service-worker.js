1174682073c1c4a2a272168e139b200fa9b2d0aa
Hassan <130254959+Hassan01262011@users.noreply.github.com>
2026-09-13 00:27:59 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-const C="dead-god-v1",F=["./","./index.html","./style.css","./app.js","./data.js","./manifest.json"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="tboi-tracker-v2",F=["./","./index.html","./style.css","./app.js","./data.js","./manifest.json"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
